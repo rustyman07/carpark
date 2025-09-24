ab41753bda549b190cd494982cb94354d094bd7c
rusty <mendozakean12@gmail.com>
2025-09-25 07:27:10 +0800

    bg

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -5,7 +5,7 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
+        class="background-container bg-gray-100/30 bg-[url(/images/carpark.png)]  bg-blend-lighten flex min-h-screen flex-col items-center  pt-6 sm:justify-center sm:pt-0"
     >
         <!-- <div>
             <Link href="/">
@@ -14,9 +14,23 @@ import { Link } from '@inertiajs/vue3';
         </div> -->
 
         <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
+            class="mt-6  bg-w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
         >
             <slot />
         </div>
     </div>
 </template>
+
+<!-- <style> 
+
+.background-container::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.1); /* Semi-transparent black */
+  z-index: -1; /* Pushes the overlay behind the content but in front of the background image */
+}
+</style> -->
